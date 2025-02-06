3ab4b6e4280c08e821d98cc32db5d57d46ac1ad1
mango7006 <magnus.van.der.wal@outlook.com>
2025-02-06 21:56:13 +0100

    Script Upload

--- scripts/script.sh
+++ scripts/script.sh
@@ -2,29 +2,29 @@
 
 source secret.sh
 
-repo_config=/home/mango/gits/dotfiles/config/
-repo_home=/home/mango/gits/dotfiles/home/
-repo_scripts=/home/mango/gits/dotfiles/scripts
+repo_config=~/gits/dotfiles/config/
+repo_home=~/gits/dotfiles/home/
+repo_scripts=~/gits/dotfiles/scripts
 
 echo "Updating github repo for Dotfiles"
 
-sudo cp /home/mango/.zshrc $repo_home
-sudo cp /home/mango/.wezterm.lua $repo_home
+sudo cp ~/.zshrc $repo_home
+sudo cp ~/.wezterm.lua $repo_home
 
-sudo cp /home/mango/.config/starship.toml $repo_config
-sudo cp -r /home/mango/.config/dunst/ $repo_config
-sudo cp -r /home/mango/.config/fastfetch/ $repo_config
-sudo cp -r /home/mango/.config/hypr/ $repo_config
-sudo cp -r /home/mango/.config/nvim/ $repo_config
-sudo cp -r /home/mango/.config/waybar/ $repo_config
-sudo cp -r /home/mango/.config/wofi/ $repo_config
+sudo cp ~/.config/starship.toml $repo_config
+sudo cp -r ~/.config/dunst/ $repo_config
+sudo cp -r ~/.config/fastfetch/ $repo_config
+sudo cp -r ~/.config/hypr/ $repo_config
+sudo cp -r ~/.config/nvim/ $repo_config
+sudo cp -r ~/.config/waybar/ $repo_config
+sudo cp -r ~/.config/wofi/ $repo_config
 
-sudo cp /home/mango/script.sh $repo_scripts
+sudo cp ~/script.sh $repo_scripts
 
 read -r -p "Do you want to push to github? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
   echo "Pushing to github"
-  cd /home/mango/gits/dotfiles/
+  cd ~/gits/dotfiles/
   git add .
   git commit -m "Script Upload"
   git push $gittoken@github.com/mango7006/dotfiles.git
